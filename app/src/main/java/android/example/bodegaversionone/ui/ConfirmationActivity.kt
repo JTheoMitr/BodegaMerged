f5188d34352cd747a2a849f312b6fe44d98d45cb
John <jmitropoulos@alumni.berklee.edu>
2022-03-29 11:54:35 -0500

    now successfully saving user data to the cloud

--- app/src/main/java/android/example/bodegaversionone/ui/ConfirmationActivity.kt
+++ app/src/main/java/android/example/bodegaversionone/ui/ConfirmationActivity.kt
@@ -3,9 +3,15 @@ package android.example.bodegaversionone.ui
 import android.example.bodegaversionone.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_confirmation.*
 
 class ConfirmationActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
@@ -14,5 +20,20 @@ class ConfirmationActivity : AppCompatActivity() {
         val lastName = intent.getStringExtra("lastName")
         fnameResult.text = firstName
         lnameResult.text = lastName
+
+        val user = hashMapOf(
+            "first" to firstName,
+            "last" to lastName
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("db", "Error adding document", e)
+            }
     }
 }
