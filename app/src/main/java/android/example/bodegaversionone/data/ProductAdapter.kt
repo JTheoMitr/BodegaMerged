d728d1c4262803d2f7339f294af567c0ab025670
Derek Ellis <derekwellis@outlook.com>
2022-03-31 09:41:17 -0500

    Adds ProductActivity and its layout Adds OnClickListener to ProductAdapter title Updates the manifest for the new Activity

--- app/src/main/java/android/example/bodegaversionone/data/ProductAdapter.kt
+++ app/src/main/java/android/example/bodegaversionone/data/ProductAdapter.kt
@@ -1,37 +1,64 @@
 package android.example.bodegaversionone.data
 
 import android.content.Context
+import android.content.Intent
+import android.example.bodegaversionone.MainActivity
 import android.example.bodegaversionone.R
 import android.example.bodegaversionone.models.Product
+import android.example.bodegaversionone.ui.ProductActivity
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ListAdapter
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.row_items.view.*
+import kotlin.coroutines.coroutineContext
+import android.widget.Toast.makeText as makeText1
 
 class ProductAdapter(context : Context, val productList : List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
-    class ProductViewHolder(productView : View) : RecyclerView.ViewHolder(productView) {
+    class ProductViewHolder(productView : View) : RecyclerView.ViewHolder(productView), View.OnClickListener {
+
+        private val TAG = javaClass.simpleName
 
         var title: TextView
         var description: TextView
         var image: ImageView
 
+
+
         init {
             title = productView.title
             description = productView.description
             image = productView.imageProduct
+
+            title.setOnClickListener {
+                val intent = Intent(productView.context, ProductActivity::class.java)
+                val bund = Bundle()
+                startActivity(productView.context, intent, bund)
+
+            }
         }
+
+
 
         companion object {
             fun create(parent : ViewGroup) : ProductViewHolder {
                 val view : View = LayoutInflater.from(parent.context).inflate(R.layout.row_items, parent, false)
                 return ProductViewHolder(view)
             }
+        }
+
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
         }
     }
 
@@ -48,6 +75,8 @@ class ProductAdapter(context : Context, val productList : List<Product>) : Recyc
         Glide.with(holder.image)
             .load(url)
             .into(holder.image)
+
+
     }
 
 
