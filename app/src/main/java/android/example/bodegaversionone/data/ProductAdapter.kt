4085df48970222ba5dd73f17c1e21ccbde974e9d
Derek <derekwellis@outlook.com>
2022-03-31 09:52:42 -0500

    Added ProductActivity and its layout Updated ProductAdapter with OnClickListener Changes to Manifest for new activity Kotlin Gradle Plugin is updated from 1.5.0 to 1.6.10 (watch for errors)

--- app/src/main/java/android/example/bodegaversionone/data/ProductAdapter.kt
+++ app/src/main/java/android/example/bodegaversionone/data/ProductAdapter.kt
@@ -1,21 +1,29 @@
 package android.example.bodegaversionone.data
 
 import android.content.Context
+import android.content.Intent
 import android.example.bodegaversionone.R
 import android.example.bodegaversionone.models.Product
+import android.example.bodegaversionone.ui.ProductActivity
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ListAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.row_items.view.*
 
 class ProductAdapter(context : Context, val productList : List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
-    class ProductViewHolder(productView : View) : RecyclerView.ViewHolder(productView) {
+    class ProductViewHolder(productView : View) : RecyclerView.ViewHolder(productView), View.OnClickListener {
+
+        private var TAG = javaClass.simpleName
+
 
         var title: TextView
         var description: TextView
@@ -25,6 +33,14 @@ class ProductAdapter(context : Context, val productList : List<Product>) : Recyc
             title = productView.title
             description = productView.description
             image = productView.imageProduct
+
+            title.setOnClickListener {
+                val intent = Intent(productView.context, ProductActivity::class.java)
+                val bund = Bundle()
+                startActivity(productView.context, intent, bund)
+                Log.d(TAG, "Clicked!")
+            }
+
         }
 
         companion object {
@@ -32,6 +48,10 @@ class ProductAdapter(context : Context, val productList : List<Product>) : Recyc
                 val view : View = LayoutInflater.from(parent.context).inflate(R.layout.row_items, parent, false)
                 return ProductViewHolder(view)
             }
+        }
+
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
         }
     }
 
