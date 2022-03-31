b58ec79eb6f6cb43a88ac4effa5da2b0645237dd
Derek Ellis <derekwellis@outlook.com>
2022-03-31 13:16:51 -0500

    Working product view activity

--- app/src/main/java/android/example/bodegaversionone/ui/ProductActivity.kt
+++ app/src/main/java/android/example/bodegaversionone/ui/ProductActivity.kt
@@ -1,14 +1,36 @@
 package android.example.bodegaversionone.ui
 
 import android.example.bodegaversionone.R
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
 
 class ProductActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = javaClass.simpleName
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
+
+
+        val title = intent.getStringExtra("title")
+        val description = intent.getStringExtra("description")
+        val image = intent.getParcelableExtra<Bitmap>("image")
+
+        val tvTitle = findViewById<TextView>(R.id.tvProductViewTitle)
+        val tvDesc = findViewById<TextView>(R.id.tvProductViewDescription)
+        val img = findViewById<ImageView>(R.id.imageView)
+
+        tvTitle.text = title
+        tvDesc.text = description
+        img.setImageBitmap(image)
+
     }
 
     fun addToCart(view: View) {}
